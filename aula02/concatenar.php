d42562ef96faec2c8ee646000e8888f4d17f6de1
Vinicius Oliveira <vinixoddias@gmail.com>
2025-02-26 10:28:38 -0300

    Aula sobre concatenação de variáveis

---
+++ aula02/concatenar.php
@@ -0,0 +1,13 @@
+<?php
+    ECHO "Hello World!<br>";
+    echo "Hello World!<br>";
+    EcHo "Hello World!<br>";
+
+    $nome = "Vinicius";
+    $sobrenome = "Oliveira";
+    $numero = 4;
+
+    echo "${nome} <h1> ${sobrenome} </h1>";
+    echo $numero . "<p> " . $nome . 
+    " " . $sobrenome . "<p>";
+?>
