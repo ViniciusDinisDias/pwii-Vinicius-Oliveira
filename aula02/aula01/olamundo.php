eaf2a41f15a9080df00d15f9f73f213df9f51bf3
Vinicius Oliveira <vinixoddias@gmail.com>
2025-02-27 07:23:51 -0300

    Arquivos aula 1 PHP

---
+++ aula02/aula01/olamundo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <style>
+        .texto {
+            size: 5rem;
+        }
+    </style>
+    <body>
+
+    <?php
+    echo "My firsy PHP script";
+    ?>
+
+    </body>
+</html>
