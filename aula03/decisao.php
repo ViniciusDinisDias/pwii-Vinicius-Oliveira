2b0772e440fc80cecd75dbac7b7124b1a70e10c2
Vinicius Oliveira <vinixoddias@gmail.com>
2025-03-12 10:34:03 -0300

    Continuação aula 3

---
+++ aula03/decisao.php
@@ -0,0 +1,21 @@
+<?php
+$n = 6;
+
+if ($n >= 4) {
+    echo $n, " é maior ou igual a 4";
+    echo "<br>";
+}
+
+$n1 = 3;
+$n2= 4;
+
+$result = ($n1%$n2)%2;
+
+if ($result == 1) {
+    echo "<br>", $result. " quociente impar";
+} else {
+    echo "<br>", $result, " quociente de par.";
+}
+?>
+
+
