7104aadc52d7dee14cb9eeb4331f63091c091776
Vinicius Oliveira <vinixoddias@gmail.com>
2025-04-09 10:32:05 -0300

    Aula sobre funções em PHP

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # pwii-Vinicius-Oliveira
 Porgramação Web II por Professor João Siles e Najara Cardoso
+Dupla: Vinicius Oliveira e Matheus Holanda
---
+++ aula05/funcao.php
@@ -0,0 +1,18 @@
+<?php
+    function myMessage() {
+        echo "Hello World! <br>";
+    }
+
+    myMessage(); 
+
+
+    function Soma($n1, $n2) {
+        echo $n1 + $n2, "<br>";
+    }
+
+    Soma(3,4);
+
+    function cadastar() {
+        //vai ser uma lógica para cadastro
+    }
+?>
